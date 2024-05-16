6f82924a78be10165c1a62e29c1ee357a0dd22fe
HRamil <ramilhasanov2000@gmail.com>
2024-05-17 00:16:52 +0300

    son

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,17 +5,22 @@ import './blogPage.scss'
 
 //image
 import banner from '@/images/blogListBanner.jpg'
+import BlogComponent from '@/components/blogComponent'
 
 export default function page() {
     return (
         <div className='blogPage'>
             <div className='banner'>
                 <h1>
-                    
+                    Blog
                 </h1>
                 <div className='image'>
                     <Image src={banner} alt='blog image' />
                 </div>
+            </div>
+
+            <div className='container'>
+                <BlogComponent />
             </div>
         </div>
     )
