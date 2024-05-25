a492086eb5b3c05f660527810533ce47bfbf7383
HRamil <ramilhasanov2000@gmail.com>
2024-05-25 10:40:52 +0300

    models

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['localhost', '158.220.95.65:8000']
+    }
+}
 
-export default nextConfig;
+module.exports = nextConfig
